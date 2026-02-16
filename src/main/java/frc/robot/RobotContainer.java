8744324a154b8eb129397cc11b5c1e03f27689cb
Daniel Hoffmaster <daniel.m.hoffmaster@gmail.com>
2026-02-16 13:03:02 -0600

    Added Limelight helpers

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,7 +17,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -43,45 +42,42 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
-
     autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData("Auto Chooser", autoChooser);
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
+
+    //Driver Controls
+
+    //Intake
     driver.leftTrigger().onTrue(intakePivot.ifNotDownPutDown().andThen(new ParallelCommandGroup(intakeRollers.run(1), treadmill.run(1))));
     driver.leftTrigger().onFalse(new ParallelCommandGroup(intakeRollers.stop(), treadmill.stop()));
+    
+    //Drop Intake
     driver.povDown().onTrue(intakePivot.dropIntake());
 
+
+    //Operator Controls
+
+    //Run Indexer --> Shoot ball
     operator.rightTrigger().whileTrue(indexer.run(1));
+    operator.rightTrigger().onFalse(indexer.stop());
+    
+
+    //Climber Up
     operator.leftBumper().onTrue(climber.climberUp());
-    //operator.leftBumper().onFalse();
+    operator.leftBumper().onFalse(climber.stop());
+    
+    //Climber Down
     operator.rightBumper().onTrue(climber.climberDown());
-    //operator.rightBumper().onFalse();
+    operator.rightBumper().onFalse(climber.stop());
 
 
   }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-
    
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
     return autoChooser.getSelected();
   }
 }
