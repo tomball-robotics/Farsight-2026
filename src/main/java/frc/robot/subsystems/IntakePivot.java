63a7f055391052a5e6fdfbd30807422c2346ad83
Kegan Cherry <kegan.cherry@outlook.com>
2026-09-10 07:39:50 -0500

    re added shotsolution (oops)

--- src/main/java/frc/robot/subsystems/IntakePivot.java
+++ src/main/java/frc/robot/subsystems/IntakePivot.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.controls.CoastOut;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
